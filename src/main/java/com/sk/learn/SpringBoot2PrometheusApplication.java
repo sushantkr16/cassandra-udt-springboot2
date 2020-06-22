961b3e8c22f2dddf168c4d3a2dd6c85962e5eab7
skumar47 <skumar47@jcp.com>
2020-06-22 17:16:30 +0530

    initial commit=

--- src/main/java/com/sk/learn/SpringBoot2PrometheusApplication.java
+++ src/main/java/com/sk/learn/SpringBoot2PrometheusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot2DockerApplication {
+public class SpringBoot2PrometheusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot2DockerApplication.class, args);
+		SpringApplication.run(SpringBoot2PrometheusApplication.class, args);
 	}
 
 }
